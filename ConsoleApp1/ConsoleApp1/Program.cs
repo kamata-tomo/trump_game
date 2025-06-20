39739867fe323abfc214bf14fd94713cb68acace
kamata-tomo <241205@st.yoshida-g.ac.jp>
2025-06-20 11:23:18 +0900

    #1　コンソールで、1～4の数字を4回入力できるようにする 対応中

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            for(int i=0; i <4; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine($"{i+1}番目のカードを入力してください(1-4) > ");
+
+                    if(!int.TryParse(Console.ReadLine(), out int input))
+                    {
+                        Console.WriteLine("1-4の数字を入力してください");
+                        
+                        continue;
+                    }
+                    if (input < 1 || input > 4)
+                    {
+                        Console.WriteLine("1-4の間で数字を入力してください");
+                    }
+                }
+            }
         }
     }
 }
